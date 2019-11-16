00b9b89941583bca3549606d80a8c1b2a4439b02
kendrang <kendrang@users.noreply.github.com>
2019-11-16 11:29:50 -0500

    set up constructor

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    private T[] input;
+
+    public ArrayUtility (T[] arr){
+        this.input = arr;
+
+    }
+
+
+    public Integer getNumberOfOccurrences(){
+
+    }
 }
