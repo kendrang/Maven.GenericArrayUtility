5926fd5f2dfda6e90df53ab823e6170b67bf6720
kendrang <kendrang@users.noreply.github.com>
2019-11-16 12:10:12 -0500

    one more to go

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,6 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * Created by leon on 3/6/18.
@@ -11,11 +13,33 @@ public class ArrayUtility<T> {
 
     public ArrayUtility (T[] arr){
         this.input = arr;
-
     }
 
 
-    public Integer getNumberOfOccurrences(){
+    public Integer getNumberOfOccurrences(T valueToEvaluate){
+       return Math.toIntExact(Arrays.stream(input)
+               .filter(element -> element.equals(valueToEvaluate))
+               .count());
 
+    }
+
+    public T[] removeValue (T valueToRemove){
+       return Arrays.stream(input)
+                .filter(element -> element != valueToRemove)
+                .toArray(result ->Arrays.copyOf(input,result));
+
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+
+
+        return null;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+       input = Stream.concat(Arrays.stream(input), Arrays.stream(arrayToMerge))
+                .toArray(size -> Arrays.copyOf(input, size));
+
+       return getNumberOfOccurrences(valueToEvaluate);
     }
 }
